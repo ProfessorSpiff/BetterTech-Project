e5e62356c74690008a76ed2c37487b3a772cefb4
CHRISTOPHER PIETERSE <215079941@student.uj.ac.za>
2018-09-25 19:49:44 +0200

    Report Gross works now

--- BetterTech Webpage/BetterTech Webpage/ReportGross.aspx.cs
+++ BetterTech Webpage/BetterTech Webpage/ReportGross.aspx.cs
@@ -11,7 +11,27 @@ namespace BetterTech_Webpage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var db = new DataLinqDataContext();
 
+            Decimal grossRevenue = 0;
+            dynamic items = from c in db.Items
+                            select c;
+            foreach (Item d in items)
+            {
+                grossRevenue += d.Item_Qty * d.Item_SellingPrice;
+            }
+             grossRev.InnerHtml = "<h2 class=uppercase mb-30>Gross Revenue: " + grossRevenue + "</h2>";
+            Decimal recentRevenue = 0;
+            dynamic invoices = from v in db.Invoices
+                            select v;
+            foreach (Invoice inv in invoices)
+            {
+                if (inv.Invoice_Date > DateTime.Now.AddMonths(-3))
+                {
+                    recentRevenue += inv.Invoice_Total;
+                }
+            }
+            recentRev.InnerHtml = "<h2 class=uppercase mb-30>Revenue from past 3 months: " + recentRevenue + "</h2>";
         }
     }
 }
